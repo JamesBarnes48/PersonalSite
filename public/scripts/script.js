f8c41d4a0e473e619fa3cc1f0c02842f381d1456
James Barnes <jamesbarnes20082000@gmail.com>
2021-03-24 16:40:46 +0000

    added documentation to portfolio

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,6 +2,7 @@
 
 var homeform = document.getElementById("home-form");
 var showcaseform = document.getElementById("showcase-form");
+var showcaseform2 = document.getElementById("showcase-form-2");
 
 document.getElementById("home-link").addEventListener("click", function () {
   homeform.submit();
@@ -9,4 +10,8 @@ document.getElementById("home-link").addEventListener("click", function () {
 
 document.getElementById("showcase-link").addEventListener("click", function () {
   showcaseform.submit();
+});
+
+document.getElementById("showcase-link-2").addEventListener("click", function () {
+  showcaseform2.submit();
 });
